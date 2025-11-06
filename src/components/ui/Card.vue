54bf24b32e6734c7aebd05081fb60847e395a340
Naufal rizki rabbani <naufal.rabbani132@gmail.com>
2025-11-06 14:27:07 +0700

    Add files via upload

--- src/components/ui/Card.vue
+++ src/components/ui/Card.vue
@@ -1,20 +1,19 @@
 <template>
-  <div class="card bg-base-100 w-96 shadow-sm ">
-  <figure>
-    <img
-      src="../../../public/artikel1.jpg"
-      alt="Shoes" />
-  </figure>
-  <div class="card-body w-full  border-black border-2">
-    <h2 class="card-title">kucing</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary" href="artikel.html">Buy Now</button>
+  <div class="card bg-base-100 w-96 shadow-sm">
+    <figure>
+      <slot name="image"></slot>
+    </figure>
+    <div class="card-body">
+      <slot name="title"></slot>
+      
+      <slot name="content"></slot>
+      
+      <div class="card-actions justify-end">
+        <slot name="actions"></slot>
+      </div>
     </div>
   </div>
-</div>
 </template>
-
 <script setup>
 // Tidak perlu script, semua sudah di handle oleh props/slots
 </script>
